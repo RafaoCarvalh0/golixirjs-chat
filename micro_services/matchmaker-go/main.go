70d370a53ad6d479964b9e1e811574cabae918eb
Rafael Carvalho <az.carta@gmail.com>
2025-07-28 19:25:10 -0300

    feat: add JWT authentication middleware and update match creation endpoint

--- micro_services/matchmaker-go/main.go
+++ micro_services/matchmaker-go/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"matchmaker-go/internal/adapters/in/http"
+	"matchmaker-go/internal/adapters/in/http/middleware"
 	"matchmaker-go/internal/domain"
 
 	"github.com/gin-gonic/gin"
@@ -11,10 +12,12 @@ func main() {
 	var waitingQueue domain.UserQueue
 
 	router := gin.Default()
+
 	router.Use(gin.Recovery())
+	router.Use(middleware.JWTAuthMiddleware())
 
 	handler := http.NewMatchmakerHandler(waitingQueue)
-	router.POST("/matchmake", handler.HandleMatchmaking)
+	router.POST("/create-match", handler.HandleMatchmaking)
 
 	router.Run(":8000")
 }
